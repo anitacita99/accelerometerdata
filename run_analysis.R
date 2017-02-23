2cf6d173e146317ba2c36417d36b47bb5c5ff994
Anita Stout <anitacita99@gmail.com>
2017-02-23 15:41:28 -0500

    Improved variable names for better readability.

--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,44 @@
+library(plyr)
+library(dplyr)
+
 # download the raw data zip file and unzip
 # download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "rawData.zip")
 # unzip("rawData.zip")
 
 # read test and training data
-subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = c("subjectId"))
 dataTest <- read.table("UCI HAR Dataset/test/X_test.txt")
-dataLablesTest <- read.table("UCI HAR Dataset/test/y_test.txt")
+dataActivityIdsTest <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = c("activityId"))
 
-subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = c("subjectId"))
 dataTrain <- read.table("UCI HAR Dataset/train/X_train.txt")
-dataLablesTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+dataActivityIdsTrain <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = c("activityId"))
 
 # 1. Merges the training and the test sets to create one data set. 
 subject <- bind_rows(subjectTest, subjectTrain)
-colnames(subject) <- c("subjectId")
-data <- bind_rows(dataTest, dataTrain)
-dataLables <- bind_rows(dataLablesTest, dataLablesTrain)
-colnames(dataLables) <- c("activityId")
+dataAll <- bind_rows(dataTest, dataTrain)
+dataActivityIds <- bind_rows(dataActivityIdsTest, dataActivityIdsTrain)
 
-# read list of feature variables and select just the mean or standard deviation variables
-features <- read.table("UCI HAR Dataset/features.txt")
-meanOrStdCol <- features[grep("mean\\(\\)|std\\(\\)", features$V2, ignore.case = TRUE),]
+# read list of feature variables and select just the mean and standard deviation features
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("featureId", "featureName"))
+meanAndStdDevFeatures <- features[grep("mean\\(\\)|std\\(\\)", features$featureName, ignore.case = TRUE),]
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-meanOrStdData <- data[,meanOrStdCol$V1]
+dataMeanAndStdDevOnly <- dataAll[,meanAndStdDevFeatures$featureId]
 
 # 3. Uses descriptive activity names to name the activities in the data set 
-activityDict <- read.table("UCI HAR Dataset/activity_labels.txt")
-activityLbls <- mapvalues(as.character(dataLables$activityId), from = as.character(activityDict$V1), to = as.character(activityDict$V2))
+activityDict <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("activityId", "activityName"))
+dataActivityNames <- mapvalues(as.character(dataActivityIds$activityId), from = as.character(activityDict$activityId), to = as.character(activityDict$activityName))
 
 # 4. Appropriately labels the data set with descriptive variable names. 
-colnames(meanOrStdData) <- as.vector(meanOrStdCol$V2)
+colnames(dataMeanAndStdDevOnly) <- as.vector(meanAndStdDevFeatures$featureName)
 
-# unify the mean and std data into a single table
-answer1 <- bind_cols(subject, as.data.frame(activityLbls), meanOrStdData)
+# unify the columns into a single table: subject Ids, activity names, and mean and std dev data
+answer1MeanAndStdDevData <- bind_cols(subject, as.data.frame(dataActivityNames), dataMeanAndStdDevOnly)
+names(answer1MeanAndStdDevData)[1]<-"subject"
+names(answer1MeanAndStdDevData)[2]<-"activity"
 
 # 5. From the data set in step 4, creates a second, independent tidy data set 
 #    with the average of each variable for each activity and each subject.
-answer2 <- group_by(answer1, activityLbls, subjectId, add = TRUE) %>%
+answer2MeanAndStdDevAvgs <- group_by(answer1MeanAndStdDevData, activity, subject, add = TRUE) %>%
   summarise_all(mean)
